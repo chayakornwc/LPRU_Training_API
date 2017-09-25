64f3e06aed74d62d54684551efe523bc94c13e59
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2017-09-25 19:04:32 +0700

    a

--- node/training_manager/src/index.js
+++ node/training_manager/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Layout extends Component {
+  render() {
+    return (
+      <h1>www</h1>
+    );
+  }
+}
+  const root = document.getElementById('root');
+export default Layout;
+ReactDOM.render(<layout/>, root);
